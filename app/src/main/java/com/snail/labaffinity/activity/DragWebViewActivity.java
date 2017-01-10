c6bdcefaa7c509e48fcc5ddd907573839dde0f73
happylishang <happylishang@163.com>
2017-01-10 15:29:38 +0800

    site

--- app/src/main/java/com/snail/labaffinity/activity/DragWebViewActivity.java
+++ app/src/main/java/com/snail/labaffinity/activity/DragWebViewActivity.java
@@ -3,7 +3,9 @@ package com.snail.labaffinity.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.snail.labaffinity.R;
 
@@ -20,6 +22,11 @@ public class DragWebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drag_list_webview);
         WebView webView= (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
         webView.loadUrl("http://happylishang.github.io/");
     }
 }
--- app/src/main/java/com/snail/labaffinity/activity/DragWebViewListActivity.java
+++ app/src/main/java/com/snail/labaffinity/activity/DragWebViewListActivity.java
@@ -3,7 +3,9 @@ package com.snail.labaffinity.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.snail.labaffinity.R;
 import com.snail.labaffinity.view.DragScrollDetailsLayout;
@@ -21,6 +23,10 @@ public class DragWebViewListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drag_webview_list);
         WebView webView= (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("http://happylishang.github.io/");
         DragScrollDetailsLayout md= (DragScrollDetailsLayout) findViewById(R.id.drag_lv);
         md.setPercent(0);
